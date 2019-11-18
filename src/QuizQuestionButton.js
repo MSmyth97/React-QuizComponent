a5bdcd117ec7bd31205723117c36636f9e35cc6e
MSmyth97 <mathew.smyth@outlook.com>
2019-11-18 11:52:01 +0000

    added next quiz question functionality

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
   handleClick = () => {
-    this.props.clickHandler({ button_text });
-    if (button_text === this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler();
-    }
+    this.props.clickHandler(this.props.button_text);
   };
 
   render() {
